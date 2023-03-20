f2884930b662dc2f47fbacae110ce7ed5e54ea43
tutoh <gicherudianagee@gmail.com>
2023-03-20 08:49:44 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main(void) {
+    printf("Roses are red,\n");
+    printf("Violets are blue,\n");
+    printf("Sugar is sweet,\n");
+    printf("And so are you.\n");
+    return 0;
+}
