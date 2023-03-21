382b4a23187c3db2269e87773e78f0bc527640e1
tutoh <gicherudianagee@gmail.com>
2023-03-21 11:21:35 -0700

    _putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Return Always 0
+ * Return: Always 0
  */
 
 int main(void)
@@ -16,6 +16,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\');
-	return(0):
+	_putchar('\n');
+	return(0);
 }		
