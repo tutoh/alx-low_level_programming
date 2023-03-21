6b75a0c0f10af8244a680c6bf99a6c06cbd71021
tutoh <gicherudianagee@gmail.com>
2023-03-21 14:38:38 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of te day of jack bauer
+ * Jack_bauer - prints every minute of the day of Jack bauer
  * Return: ...
  */
 
-void jack_bauer(void)
+void Jack_bauer(void)
 
 {
 
 	int a, b, c, d;
 
-	for (a = 0; a <= 2: a++)
+	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
 	{
